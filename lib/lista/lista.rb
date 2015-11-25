7e305545cc5ebe1037ffeb4fc6bb0fd16d44a4e3
Juan Nicolas <juannicolasvargasmoreno@hotmail.com>
2015-11-25 16:11:16 +0000

    arreglado errores en lista y añadido enumerable

--- lib/lista/lista.rb
+++ lib/lista/lista.rb
@@ -1,38 +1,50 @@
 Node = Struct.new(:value,:next,:prev)
 
 class Lista
+	include Enumerable
 	attr_reader :head, :tail
 	def initialize()
-		@head
-		@tail
+		@head = nil
+		@tail = nil
 	end
 
+	def push(value)
+		if @head == nil
+	@head = Node.new(value,nil,nil)
+			@tail = @head
+
+		else
+			aux = @head
+			while aux.next != nil
+				aux = aux.next
+			end
+			#mirar prev en push.
+			aux.next = Node.new(value,nil,@tail)
+			@tail = @tail.next
+		
+		end	
+	end
+
+
+
 	def pop()
-		if @head != nil
+		if @head == nil
+			return nil
+		elsif @head == @tail
+			aux = @head
+			@head = nil
+			@tail = nil
+			return aux			
+		else
 			aux = @head
 			@head = @head.next
 			@head.prev = nil
-			return aux
-			else
-				return nil
+			return aux		
 		end
 	end
 
 	
-	def push(value)
-		if @head !=nil
-			@tail = @head
-			while @tail.next != nil
-				@tail = @tail.next
-			end
-			#mirar prev en push.
-			@tail.next = Node.new(value,@tail,nil)
-			@tail = @tail.next
-		else
-			@head = Node.new(value,nil,nil)
-			@tail = @head
-		end	
-	end
+
 
 
 
